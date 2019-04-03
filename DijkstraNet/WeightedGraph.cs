58eebabd8a85d48f81b39a80c6ba8b1ed55dac1b
Ed Casillas <eduardo_casillas@hotmail.com>
2019-04-03 09:47:04 -0600

    Including functionality to add vertices and edges to the graph.

--- DijkstraNet/WeightedGraph.cs
+++ DijkstraNet/WeightedGraph.cs
@@ -5,20 +5,20 @@ namespace DijkstraNet
 {
 	public class WeightedGraph<TData, TWeight>
 	{
-		private readonly IDictionary<TData, WeightedGraphNode<TData, TWeight>> nodes = new Dictionary<TData, WeightedGraphNode<TData, TWeight>>();
+		private readonly IDictionary<TData, ICollection<WeightedEdge<TData, TWeight>>> nodes = new Dictionary<TData, ICollection<WeightedEdge<TData, TWeight>>>();
 		private readonly ICollection<WeightedEdge<TData, TWeight>> edges = new List<WeightedEdge<TData, TWeight>>();
 
 		public void AddNode(TData data)
 		{
 			if(nodes.ContainsKey(data)) throw new InvalidOperationException("Data already exists in the graph.");
-			nodes.Add(data, new WeightedGraphNode<TData, TWeight>(data));
+			nodes.Add(data, new List<WeightedEdge<TData, TWeight>>());
 		}
 
 		public void AddEdge(TData from, TData to, TWeight weight, bool directed = true)
 		{
 			if(!nodes.ContainsKey(from)) AddNode(from);
 			if (!nodes.ContainsKey(to)) AddNode(to);
-			nodes[from].Edges.Add(new WeightedEdge<TData,TWeight>(nodes[from], nodes[to], weight));
+			//nodes[from].Add(new WeightedEdge<TData,TWeight>(from, to, weight));
 			if (!directed) AddEdge(to, from, weight);
 		}
 	}
